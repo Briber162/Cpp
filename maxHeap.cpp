17967d98ba20558367a8e5bd2a2a20ed38870797
KUNAL CHAUDHARY <briber162@gmail.com>
2025-08-18 22:31:23 +0530

    Create maxHeap.cpp

---
+++ maxHeap.cpp
@@ -0,0 +1,59 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <queue>
+#include <chrono>
+#include<vector>
+
+struct Bid{
+    double bid_price;
+    int bid_id;
+   int timestamp;
+};
+
+// Overload << for printing Bid
+std::ostream& operator<<(std::ostream& os, const Bid& b) {
+    os << "{id: " << b.bid_id 
+       << ", price: " << b.bid_price 
+       << ", ts: " << b.timestamp << "}";
+    return os;
+}
+
+struct comparator{
+    bool operator() (Bid& a, Bid& b){
+        std::cout<<a.bid_price <<" "<<b.bid_price<<"\n";
+        if(a.bid_price == b.bid_price){
+            return a.timestamp > b.timestamp;
+        }
+        return a.bid_price < b.bid_price;
+    }
+};
+
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro\n";
+    
+    
+    std::priority_queue<int> pq;
+    pq.push(1);
+    pq.push(25);
+    pq.push(31);
+    pq.push(19);
+    
+    std::cout<<pq.top()<<"\n";
+
+    //min Heap
+    Bid b1 = {10.5, 1, 5};
+    Bid b2 = {12.5, 2, 5};
+    Bid b3 = {9.4, 3,2};
+    std::priority_queue<Bid, std::vector<Bid>, comparator> minHeap;
+    
+    minHeap.push(b1);
+    minHeap.push(b2);
+    minHeap.push(b3);
+    
+    std::cout<<minHeap.top()<<"\n";
+    minHeap.pop();
+    std::cout<<minHeap.top()<<"\n";
+     
+    return 0;
+}
